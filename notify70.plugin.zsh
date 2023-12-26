91f45ad97f7e1137b09494f131ed07083e35ce63
qilingzhao <qilingzhao8@gmail.com>
2023-12-27 05:44:27 +0800

    ping lark bot successful

--- notify70.plugin.zsh
+++ notify70.plugin.zsh
@@ -5,8 +5,25 @@
 [[ -o interactive ]] || return #interactive only!
 zmodload zsh/datetime || { print "can't load zsh/datetime"; return } # faster than date()
 autoload -Uz add-zsh-hook || { print "can't add zsh hook!"; return }
+[[ ! -z $notify70_lark_bot_url ]] ||  { print "can't find notify70_lark_bot_url in your env. To create a lark bot,\
+    ref: https://open.larksuite.com/document/client-docs/bot-v3/add-custom-bot"; return } #
 
-(( ${+bgnotify_threshold} )) || bgnotify_threshold=3 #default 3 seconds
+(( ${+notify_sec_threshold} )) || notify_sec_threshold=3 #default 3 seconds
+
+# TODO: sec_threshold by cmd prefix
+# if ! (( $+notify70_cmd_to_threshold )); then
+#   declare -A notify70_cmd_to_threshold
+#   notify70_cmd_to_threshold[blade] = 60
+# fi
+
+# functions
+
+function send_lark() {
+  curl -X POST -H "Content-Type: application/json" \
+      -d '{"msg_type":"text","content":{"text":"request example"}}' \
+      "$notify70_lark_bot_url" > /dev/null 2>&1 & # async send lark message, non-block current terminal. TODO: And no output.
+}
+
 
 ## Zsh hooks ##
 
@@ -15,16 +32,22 @@ function preexec_capture() {
   exec_cmd="$1"
 }
 
+# TODO: whitelist [vim, ...]
 function precmd_notify() {
-  didexit=$?
-  elapsed=$(( EPOCHSECONDS - exec_begin_timestamp ))
-  past_threshold=$(( elapsed >= bgnotify_threshold ))
-  if (( exec_begin_timestamp > 0 )) && (( past_threshold )); then
-    print "elapsed: $elapsed"
-    print "didexit: $didexit"
-    print "exec_cmd: $exec_cmd"
-  fi
-  exec_begin_timestamp=0 #reset it to 0!
+  {
+    local exit_status=$?
+    local elapsed=$(( EPOCHSECONDS - exec_begin_timestamp ))
+    local is_past_threshold=$(( elapsed >= notify_sec_threshold ))
+    if (( exec_begin_timestamp > 0 )) && (( is_past_threshold )); then
+      print "[debug] elapsed: $elapsed"
+      print "[debug] exit_code: $exit_status"
+      print "[debug] exec_cmd: $exec_cmd"
+      send_lark
+    fi
+  } always {
+    exec_begin_timestamp=0 #reset it to 0!
+  }
+
 }
 
 add-zsh-hook preexec preexec_capture
